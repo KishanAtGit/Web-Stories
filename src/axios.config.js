e0bbc09e31a78fa32ba1934f7069455af0053180
Uttam <kk82171@gmail.com>
2024-09-23 00:50:02 +0530

    Added backend hosted url to apiClient

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://web-stories-backend.onrender.com/api/',
 });
 
 apiClient.interceptors.request.use(
